ae94d11ea28901b9e6bb5af32582f2f23d59130d
Gabriel Lima <glima7591@gmail.com>
2022-05-14 19:09:34 -0300

    consertando algumas propriedades de css do background image

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -5,7 +5,7 @@ export default class Description extends Component {
   render() {
     return (
       <div id='description-container'>
-        <p>Startup Framework gives you complete freedom over your creative process - you don't have to think about any technical aspects. There are no limits and absolutely no coding.</p>
+        <p>Startup Framework gives you complete freedom over your creative process — you don't have to think about any technical aspects. There are no limits and absolutely no coding.</p>
       </div>
     )
   }
